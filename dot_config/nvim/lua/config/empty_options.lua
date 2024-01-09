8aa25ecf9b1efa34150bd33d6281344675950f77
Rasmus Bertell <rasmus@bertell.fi>
2024-01-09 20:55:21 +0200

    Update .config/nvim/lua/config/options.lua

--- dot_config/nvim/lua/config/empty_options.lua
+++ dot_config/nvim/lua/config/empty_options.lua
@@ -2,3 +2,8 @@
 vim.keymap.set('n', '<Space>', '<Nop>', { silent = true })
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+-- Set absolute character limit and guides before it
+vim.opt.textwidth = 120
+vim.opt.colorcolumn = "-0,-20,-40"
+vim.opt.wrap = false
