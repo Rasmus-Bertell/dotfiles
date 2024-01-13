6785a0bbac6981ad1602dd97f29cdbbecbc8f226
Rasmus Bertell <rasmus@bertell.fi>
2024-01-13 23:13:14 +0200

    Add .config/nvim/lua/plugins/oil-nvim/init.lua

---
+++ dot_config/nvim/lua/plugins/oil-nvim/init.lua
@@ -0,0 +1,11 @@
+local M = { 'stevearc/oil.nvim' }
+
+M.opts = {
+        win_options = {
+                signcolumn = 'yes:2',
+        },
+}
+
+M.dependencies = { 'nvim-tree/nvim-web-devicons' }
+
+return M
