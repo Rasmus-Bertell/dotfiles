c4001c04b813b37fce2dd24b65025d2cc8e5ae91
Rasmus Bertell <rasmus@bertell.fi>
2024-01-16 17:46:44 +0200

    Update .config/nvim/lua/plugins/conform-nvim/init.lua

--- dot_config/nvim/lua/plugins/conform-nvim/init.lua
+++ dot_config/nvim/lua/plugins/conform-nvim/init.lua
@@ -1,5 +1,19 @@
 local M = { 'stevearc/conform.nvim' }
 
-M.opts = {}
+M.event = { "BufWritePre" }
+
+M.cmd = { "ConformInfo" }
+
+M.opts = {
+        formatters_by_ft = {}
+        format_on_save = {
+                timeout_ms = 500,
+                lsp_fallback = true
+        },
+}
+
+M.init = function()
+        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+end
 
 return M
