e00d3bea49603aafb33ac6c89168d06460adf5ab
Rasmus Bertell <rasmus@bertell.fi>
2024-01-17 19:14:16 +0200

    Update .config/nvim/lua/plugins/oil-nvim/init.lua

--- dot_config/nvim/lua/plugins/oil-nvim/init.lua
+++ dot_config/nvim/lua/plugins/oil-nvim/init.lua
@@ -1,8 +1,11 @@
 local M = { 'stevearc/oil.nvim' }
 
 -- Always load the file explorer
+-- TODO: Only load it if opening a directory or with keypress
 M.lazy     = false
 M.priority = 900
+
+M.opts = {}
 
 M.dependencies = { 'nvim-tree/nvim-web-devicons' }
 
