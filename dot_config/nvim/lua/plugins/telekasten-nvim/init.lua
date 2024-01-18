a79a761333f11cc0382f046902258b53a87fa03a
Rasmus Bertell <rasmus@bertell.fi>
2024-01-18 18:47:47 +0200

    Update .config/nvim/lua/plugins/telekasten-nvim/init.lua

--- dot_config/nvim/lua/plugins/telekasten-nvim/init.lua
+++ dot_config/nvim/lua/plugins/telekasten-nvim/init.lua
@@ -2,4 +2,8 @@ local M = { 'renerocksai/telekasten.nvim' }
 
 M.dependencies = { 'nvim-telescope/telescope.nvim' }
 
+M.opts = {
+        home = vim.fn.expand('$XDG_DOCUMENTS_DIR/notes')
+}
+
 return M
