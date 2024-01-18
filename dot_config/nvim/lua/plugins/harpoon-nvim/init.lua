54a0a3df8d91907e5244707cc28557f2a5b39044
Rasmus Bertell <rasmus@bertell.fi>
2024-01-18 19:41:27 +0200

    Add .config/nvim/lua/plugins/harpoon-nvim/init.lua

---
+++ dot_config/nvim/lua/plugins/harpoon-nvim/init.lua
@@ -0,0 +1,14 @@
+local M = { 'ThePrimeagen/harpoon' }
+
+M.branch = 'harpoon2'
+
+M.dependencies = { 'nvim-lua/plenary.nvim' }
+
+M.keys = {
+        { '<leader>ha', function() require('harpoon'):list():append() end },
+        { '<leader>hl', function() require('harpoon').ui:toggle_quick_menu(require('harpoon'):list()) end },
+        { '<leader>hn', function() require('harpoon'):list():next() end },
+        { '<leader>hp', function() require('harpoon'):list():prev() end },
+}
+
+return M
