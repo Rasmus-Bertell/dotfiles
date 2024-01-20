d99e0ba3c103c844200b23e1ca21e3ad39b08dfe
Rasmus Bertell <rasmus@bertell.fi>
2024-01-20 10:35:54 +0200

    Update .config/nvim/lua/plugins/harpoon-nvim/init.lua

--- dot_config/nvim/lua/plugins/harpoon-nvim/init.lua
+++ dot_config/nvim/lua/plugins/harpoon-nvim/init.lua
@@ -7,6 +7,7 @@ M.dependencies = { 'nvim-lua/plenary.nvim' }
 M.keys = {
         { '<leader>ha', function() require('harpoon'):list():append() end },
         { '<leader>hl', function() require('harpoon').ui:toggle_quick_menu(require('harpoon'):list()) end },
+
         { '<leader>hn', function() require('harpoon'):list():next() end },
         { '<leader>hp', function() require('harpoon'):list():prev() end },
 }
