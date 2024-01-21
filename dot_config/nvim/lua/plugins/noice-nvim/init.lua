4c45f55d512c29423ef00d7cc0ff51ad36b8344b
Rasmus Bertell <rasmus@bertell.fi>
2024-01-21 21:21:12 +0200

    Add .config/nvim/lua/plugins/lspconfig-nvim/init.lua Add .config/nvim/lua/plugins/noice-nvim/init.lua Add .config/nvim/lua/plugins/usage-tracker-nvim/init.lua

---
+++ dot_config/nvim/lua/plugins/noice-nvim/init.lua
@@ -0,0 +1,31 @@
+local M = { 'folke/noice.nvim' }
+
+M.dependencies = {
+        'MunifTanjim/nui.nvim',
+        'rcarriga/nvim-notify',
+}
+
+M.event = 'VeryLazy'
+
+M.opts = {
+        lsp = {
+                override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                },
+        },
+        presets = {
+                bottom_search = true,
+                command_palette = true,
+                long_message_to_split = true,
+                lsp_doc_border = true,
+        },
+        routes = {
+                {
+                        view = "notify",
+                        filter = { event = "msg_showmode" },
+                },
+        },
+}
+
+return M
