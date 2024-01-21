4811d3c40f7691bd4cf2b5a1c72835ac79b6d5fb
Rasmus Bertell <rasmus@bertell.fi>
2024-01-21 23:44:42 +0200

    Add .config/nvim/lua/plugins/cmp-nvim/init.lua Update .config/nvim/lua/plugins/lspconfig-nvim/init.lua Add .config/nvim/lua/plugins/neodev-nvim/init.lua

--- dot_config/nvim/lua/plugins/lspconfig-nvim/init.lua
+++ dot_config/nvim/lua/plugins/lspconfig-nvim/init.lua
@@ -1,10 +1,40 @@
 local M = { 'neovim/nvim-lspconfig' }
 
+M.dependencies = {
+        'hrsh7th/nvim-cmp',
+}
+
 -- TODO: Configure this later
 M.config = function(_, opts)
         local lspconfig = require('lspconfig')
 
-        lspconfig['lua_ls'].setup(opts)
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        opts = vim.tbl_deep_extend('force', opts, { capabilities = capabilities })
+
+        lspconfig['lua_ls'].setup(vim.tbl_deep_extend('force',
+                opts,
+                {
+                        on_init = function(client)
+                                local path = client.workspace_folders[1].name
+                                if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+                                        client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+                                                Lua = {
+                                                        runtime = {
+                                                                version = 'LuaJIT'
+                                                        },
+                                                        workspace = {
+                                                                checkThirdParty = false,
+                                                                library = vim.api.nvim_get_runtime_file("", true)
+                                                        }
+                                                }
+                                        })
+
+                                        client.notify("workspace/didChangeConfiguration",
+                                                { settings = client.config.settings })
+                                end
+                                return true
+                        end
+                }))
         lspconfig['phpactor'].setup(opts)
         lspconfig['intelephense'].setup(opts)
 
