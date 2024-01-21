3b4e6df48fd72c84641c5281f9eb7863f15a4c20
Rasmus Bertell <rasmus@bertell.fi>
2024-01-21 21:27:04 +0200

    Update .config/nvim/lua/config/options.lua

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,23 +1,23 @@
 -- Change the leader key to space
 vim.keymap.set('n', '<Space>', '<Nop>', { silent = true })
-vim.g.mapleader      = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader        = ' '
+vim.g.maplocalleader   = ' '
 
 -- Set absolute character limit and guides at and before it
-vim.opt.textwidth   = 120
-vim.opt.colorcolumn = "-0,-20,-40"
-vim.opt.wrap        = false
+vim.opt.textwidth      = 120
+vim.opt.colorcolumn    = "-0,-20,-40"
+vim.opt.wrap           = false
 
 -- Use space for indenting and use 8 characters by default
-vim.opt.expandtab   = true
-vim.opt.smarttab    = true
-vim.opt.smartindent = true
-vim.opt.shiftwidth  = 8
-vim.opt.tabstop     = 8
-vim.opt.softtabstop = 8
+vim.opt.expandtab      = true
+vim.opt.smarttab       = true
+vim.opt.smartindent    = true
+vim.opt.shiftwidth     = 8
+vim.opt.tabstop        = 8
+vim.opt.softtabstop    = 8
 
 -- Use system clipboard
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard      = "unnamedplus"
 
 -- Show relative line numbers and always show sign column
 vim.opt.signcolumn     = "yes"
