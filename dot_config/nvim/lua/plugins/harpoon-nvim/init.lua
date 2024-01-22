5171e1571e40daf9a1ef1034aa3e64521ada2cb4
Rasmus Bertell <rasmus.bertell@vitecsoftware.com>
2024-01-22 08:22:38 +0200

    Update .config/nvim/lua/plugins/harpoon-nvim/init.lua

--- dot_config/nvim/lua/plugins/harpoon-nvim/init.lua
+++ dot_config/nvim/lua/plugins/harpoon-nvim/init.lua
@@ -2,14 +2,15 @@ local M = { 'ThePrimeagen/harpoon' }
 
 M.branch = 'harpoon2'
 
+M.config = function(_, opts)
+        local harpoon = require("harpoon")
+
+        harpoon:setup(opts)
+
+        vim.keymap.set('n', '<leader>ha', function() require('harpoon'):list():append() end)
+        vim.keymap.set('n', '<leader>hl', function() require('harpoon').ui:toggle_quick_menu(require('harpoon'):list()) end)
+end
+
 M.dependencies = { 'nvim-lua/plenary.nvim' }
-
-M.keys = {
-        { '<leader>ha', function() require('harpoon'):list():append() end },
-        { '<leader>hl', function() require('harpoon').ui:toggle_quick_menu(require('harpoon'):list()) end },
-
-        { '<leader>hn', function() require('harpoon'):list():next() end },
-        { '<leader>hp', function() require('harpoon'):list():prev() end },
-}
 
 return M
