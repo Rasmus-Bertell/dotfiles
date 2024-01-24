f6ca67de1d8e40659db9a4cc3ab1691d094d9d98
Rasmus Bertell <rasmus@bertell.fi>
2024-01-24 20:39:04 +0200

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/empty_keymaps.lua
+++ dot_config/nvim/lua/config/empty_keymaps.lua
@@ -1,7 +1,20 @@
--- Trouble and diagnostics
-vim.keymap.set("n", "<leader>tt", function() require("trouble").toggle() end)
-vim.keymap.set("n", "<leader>tw", function() require("trouble").toggle("workspace_diagnostics") end)
-vim.keymap.set("n", "<leader>td", function() require("trouble").toggle("document_diagnostics") end)
-vim.keymap.set("n", "<leader>tq", function() require("trouble").toggle("quickfix") end)
-vim.keymap.set("n", "<leader>tl", function() require("trouble").toggle("loclist") end)
-vim.keymap.set("n", "<leader>tr", function() require("trouble").toggle("lsp_references") end)
+vim.keymap.set('n', '<leader>tt', function() require('trouble').toggle() end, { desc = 'Toggle trouble' })
+
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Next diagnostic' })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Previous diagnostic' })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+                local map = function(mode, key, cmd, desc)
+                        vim.keymap.set(mode, key, cmd, { buffer = ev.buf, desc = desc })
+                end
+
+                map('n', '<leader>ld', vim.lsp.buf.definition, 'Go to definition')
+                map('n', '<leader>lr', vim.lsp.buf.references, 'Go to references')
+                map('n', '<leader>lh', vim.lsp.buf.hover, 'Show hover')
+                map('n', '<leader>ls', vim.lsp.buf.signature_help, 'Show signature help')
+                map('n', '<leader>lr', vim.lsp.buf.rename, 'Rename symbol')
+                map({ 'n', 'v' }, '<leader>la', vim.lsp.buf.code_action, 'Code action')
+        end,
+})
