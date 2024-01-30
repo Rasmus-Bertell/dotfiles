36616eab07b61aef3e0cb2ee122cd49a6a1e3df4
Rasmus Bertell <rasmus.bertell@vitecsoftware.com>
2024-01-30 08:30:37 +0200

    Update .config/nvim/lua/plugins/lint-nvim/init.lua

--- dot_config/nvim/lua/plugins/lint-nvim/init.lua
+++ dot_config/nvim/lua/plugins/lint-nvim/init.lua
@@ -3,6 +3,12 @@ local M = { 'mfussenegger/nvim-lint' }
 M.config = function()
         local lint = require('lint')
 
+        local phpcs = require('lint').linters.phpcs
+        local phpmd = require('lint').linters.phpmd
+
+        phpcs.cmd = "vendor/bin/phpcs"
+        phpmd.cmd = "vendor/bin/phpmd"
+
         lint.linters_by_ft = {
                 php = {
                         'php',
