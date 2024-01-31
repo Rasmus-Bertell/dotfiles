8919368926c3419b5d4d4b463963491a8f7b2778
Rasmus Bertell <rasmus.bertell@vitecsoftware.com>
2024-01-31 14:11:31 +0200

    Update .config/nvim/lua/plugins/trouble-nvim/init.lua

--- dot_config/nvim/lua/plugins/trouble-nvim/init.lua
+++ dot_config/nvim/lua/plugins/trouble-nvim/init.lua
@@ -6,8 +6,6 @@ M.opts = {
         height = 5,
         win_config = { border = 'rounded' },
         padding = false,
-        auto_open = true,
-        auto_close = true,
         use_diagnostic_signs = true,
 }
 
