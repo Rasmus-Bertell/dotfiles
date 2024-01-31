1fa85996ce90d788dfdea25a33ab84a78a029f75
Rasmus Bertell <rasmus@bertell.fi>
2024-01-31 20:38:07 +0200

    Update .config/nvim/lua/config/command.lua

--- dot_config/nvim/lua/config/command.lua
+++ dot_config/nvim/lua/config/command.lua
@@ -1,12 +1,20 @@
 vim.api.nvim_create_user_command(
         "Autoformat",
         function(args)
-                if args.args == "enable" then
-                        vim.g.enable_autoformat = true
+                local str_starts = function(String, Start)
+                        return string.sub(String, 1, string.len(Start)) == Start
                 end
 
-                if args.args == "disable" then
+                if str_starts(args.args, "enable") then
+                        vim.g.enable_autoformat = true
+
+                        return
+                end
+
+                if str_starts(args.args, "disable") then
                         vim.g.enable_autoformat = false
+
+                        return
                 end
 
                 vim.g.enable_autoformat = not vim.g.enable_autoformat
