3d990d6f482c3775800b91436f7e9b43d68a87e3
Rasmus Bertell <rasmus@bertell.fi>
2026-04-15 18:42:21 +0300

    fix(nvim): move oil.nvim keymaps next to plugin
    closes #1

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,4 +2,3 @@ vim.g.mapleader      = ' '
 vim.g.maplocalleader = '\\'
 
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
-vim.keymap.set('n', '-', function() require('oil').open() end, { desc = 'Open parent directory' })
--- nvim/plugin/oil-nvim.lua
+++ nvim/plugin/oil-nvim.lua
@@ -1,3 +1,12 @@
 vim.pack.add({ 'https://github.com/stevearc/oil.nvim' })
 
-require('oil').setup({ columns = { 'size', 'permissions' } })
+require('oil').setup({
+        columns = { 'size', 'permissions' }
+})
+
+vim.keymap.set(
+        'n',
+        '-',
+        require('oil').open,
+        { desc = 'Open parent directory' }
+)
