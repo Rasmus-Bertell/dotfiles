59de478e97b321f01551486947203a302ee842f3
Rasmus Bertell <rasmus@bertell.fi>
2026-04-15 15:56:33 +0300

    feat(nvim): use native `vim.pack.add()`
    Replaces `lazy.nvim` package manager with `vim.pack.add()` command.
    Should be fairly easy to manage with the amount of packages I have in
    mind. Need to experiment a bit how this affects the startup time.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,4 @@ vim.g.mapleader      = ' '
 vim.g.maplocalleader = '\\'
 
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+vim.keymap.set('n', '-', function() require('oil').open() end, { desc = 'Open parent directory' })
